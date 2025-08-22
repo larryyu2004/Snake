129786aeec94ebcd252bdab428eb2c4565e78e64
larryyu2004 <jiayiyu@myyahoo.com>
2025-08-22 12:18:53 +0930

    Add Snake class to manage snake movement and collision detection

---
+++ src/modules/Snake.ts
@@ -0,0 +1,93 @@
+class Snake {
+  head: HTMLElement;
+  bodies: HTMLCollection;
+  element: HTMLElement;
+
+  constructor() {
+    this.element = document.getElementById("snake")!;
+    this.head = document.querySelector("#snake > div") as HTMLElement;
+    this.bodies = this.element.getElementsByTagName("div") as HTMLCollection;
+  }
+
+  get X() {
+    return this.head.offsetLeft;
+  }
+
+  get Y() {
+    return this.head.offsetTop;
+  }
+
+  set X(value: number) {
+    if (this.X == value) {
+      return;
+    }
+
+    if (value < 0 || value > 290) {
+      // 进入判断说明蛇撞墙了
+      throw new Error("Hit Wall!");
+    }
+
+    if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft == value) {
+      if (value > this.X) {
+        // ->, still go left
+        value = this.X - 10;
+      } else {
+        // <-, still go right
+        value = this.X + 10;
+      }
+    }
+
+    this.moveBody();
+    this.head.style.left = value + "px";
+    this.checkHeadBody();
+  }
+
+  set Y(value: number) {
+    if (this.Y == value) {
+      return;
+    }
+
+    if (value < 0 || value > 290) {
+      // 进入判断说明蛇撞墙了，抛出一个异常
+      throw new Error("Hit Wall!");
+    }
+
+    if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+      if (value > this.Y) {
+        value = this.Y - 10;
+      } else {
+        value = this.Y + 10;
+      }
+    }
+
+    this.moveBody();
+    this.head.style.top = value + "px";
+    this.checkHeadBody();
+  }
+
+  addBody() {
+    this.element.insertAdjacentHTML("beforeend", "<div></div>");
+  }
+
+  moveBody() {
+    for (let i = this.bodies.length - 1; i > 0; i--) {
+      let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+      let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+
+      (this.bodies[i] as HTMLElement).style.left = X + "px";
+      (this.bodies[i] as HTMLElement).style.top = Y + "px";
+    }
+  }
+
+  checkHeadBody() {
+    for (let i = 1; i < this.bodies.length; i++) {
+      let bd = this.bodies[i] as HTMLElement;
+      if (this.X === bd.offsetLeft && this.Y === bd.offsetTop) {
+        // 进入判断说明蛇头撞到了身体，游戏结束
+        throw new Error("Hit Yourself");
+      }
+    }
+  }
+}
+
+export default Snake;
