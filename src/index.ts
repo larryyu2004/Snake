0d9765c001558c0bf0c4c2087c197006759015e7
larryyu2004 <jiayiyu@myyahoo.com>
2025-08-22 12:19:07 +0930

    Add index.ts file to initialize GameControl and import styles for Snake game

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import "./style/index.less";
+import GameControl from "./modules/GameControl";
+
+const gameContol = new GameControl();
+
+// setInterval(() => {
+//   console.log(gameContol.direction);
+// }, 1000);
